d48721ea491935ae2fe72222ee4c3ced73f077c0
Rafael Missio <rafaelmissio@iMac-de-Rafael.local>
2025-02-18 21:40:13 -0300

    First commit

---
+++ src/test/java/com/rafaelmissio/bookstoremagener/BookstoremagenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelmissio.bookstoremagener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremagenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
