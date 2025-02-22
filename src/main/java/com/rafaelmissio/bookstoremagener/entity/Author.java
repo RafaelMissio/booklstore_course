64a4db3863fd99c673f1ce1645459c576febc18b
rafaelmissio <rtmissio@gmail.com>
2025-02-22 20:46:48 -0300

    Created auditable entity

--- src/main/java/com/rafaelmissio/bookstoremagener/entity/Author.java
+++ src/main/java/com/rafaelmissio/bookstoremagener/entity/Author.java
@@ -1,15 +1,20 @@
 package com.rafaelmissio.bookstoremagener.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
+
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
 
+@Data
 @Entity
 @Table(name = "db_author")
-public class Author implements Serializable {
+public class Author extends Auditable implements Serializable {
 
-    private static final Long serialVernsioUID = 1L;
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,52 +30,6 @@ public class Author implements Serializable {
     @OneToMany(mappedBy = "author", fetch = FetchType.LAZY)
     private List<Book> books;
 
-    public Author() {
-    }
-
-    public Author(String name, int age, List<Book> books) {
-        this.name = name;
-        this.age = age;
-        this.books = books;
-    }
-
-    public Author(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public List<Book> getBooks() {
-        return books;
-    }
-
-    public void setBooks(List<Book> books) {
-        this.books = books;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -83,15 +42,5 @@ public class Author implements Serializable {
     @Override
     public int hashCode() {
         return Long.hashCode(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Author{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                ", books=" + books +
-                '}';
     }
 }
--- src/main/java/com/rafaelmissio/bookstoremagener/entity/Book.java
+++ src/main/java/com/rafaelmissio/bookstoremagener/entity/Book.java
@@ -2,14 +2,19 @@ package com.rafaelmissio.bookstoremagener.entity;
 
 
 import jakarta.persistence.*;
+import lombok.Data;
 
+import java.io.Serial;
 import java.io.Serializable;
 
+@Data
 @Entity
 @Table(name = "db_book")
-public class Book implements Serializable {
+public class Book extends Auditable implements Serializable {
 
-    private static final Long serialVersionUID = 1L;
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,83 +41,6 @@ public class Book implements Serializable {
     @ManyToOne(cascade = {CascadeType.MERGE})
     private User user;
 
-    public Book() {
-    }
-
-    public Book(String name, String isbn, int pages, int chapters, Author author, Publisher publisher, User user) {
-        this.name = name;
-        this.isbn = isbn;
-        this.pages = pages;
-        this.chapters = chapters;
-        this.author = author;
-        this.publisher = publisher;
-        this.user = user;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public int getPages() {
-        return pages;
-    }
-
-    public void setPages(int pages) {
-        this.pages = pages;
-    }
-
-    public int getChapters() {
-        return chapters;
-    }
-
-    public void setChapters(int chapters) {
-        this.chapters = chapters;
-    }
-
-    public Author getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(Author author) {
-        this.author = author;
-    }
-
-    public Publisher getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(Publisher publisher) {
-        this.publisher = publisher;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -125,16 +53,5 @@ public class Book implements Serializable {
     @Override
     public int hashCode() {
         return Long.hashCode(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", isbn='" + isbn + '\'' +
-                ", pages=" + pages +
-                ", chapters=" + chapters +
-                '}';
     }
 }
